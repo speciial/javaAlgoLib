965adff78bb945b8e42130229452833073092b19
speciial <c.christian.heidemann@gmail.com>
2021-07-15 17:14:09 +0200

    Added RedBlackTree with insert and search

--- src/main/java/io/github/speciial/datastructures/redblacktree/RedBlackTree.java
+++ src/main/java/io/github/speciial/datastructures/redblacktree/RedBlackTree.java
@@ -1,9 +1,155 @@
 package io.github.speciial.datastructures.redblacktree;
 
+import io.github.speciial.datastructures.redblacktree.RedBlackTree.Node.Color;
+
 public class RedBlackTree {
 
+    private final Node nil;
+
+    private Node root;
+
     public RedBlackTree() {
-        
+        nil = new Node(Double.NaN);
+        nil.color = Color.BLACK;
+
+        root = nil;
+    }
+
+    public Node search(double key) {
+        Node x = root;
+        while (x != null && key != x.key) {
+            if (key < x.key) {
+                x = x.left;
+            } else {
+                x = x.right;
+            }
+        }
+        return x;
+    }
+
+    public void insert(Node z) {
+        Node y = nil;
+        Node x = root;
+
+        while (x != nil) {
+            y = x;
+            if (z.key < x.key) {
+                x = x.left;
+            } else {
+                x = x.right;
+            }
+        }
+        z.parent = y;
+        if (y == nil) {
+            root = z;
+        } else if (z.key < y.key) {
+            y.left = z;
+        } else {
+            y.right = z;
+        }
+        z.left = nil;
+        z.right = nil;
+        z.color = Color.RED;
+        fixup(z);
+    }
+
+    private void fixup(Node z) {
+        while (z.parent.color == Color.RED) {                       // breaks rule 4
+            if (z.parent == z.parent.parent.left) {
+                Node y = z.parent.parent.right;                     // y = uncle
+                if (y.color == Color.RED) {                         // uncle is red: case 1
+                    z.parent.color = Color.BLACK;                   // turn parent black: fixes rule 4, breaks rule 5
+                    y.color = Color.BLACK;                          // turn uncle black: fixes rule 4, breaks rule 5
+                    z.parent.parent.color = Color.RED;              // turn grandparent red: fixes rule 5
+                    z = z.parent.parent;                            // move z up 2 levels to check violation of rule 4
+                } else {
+                    if (z == z.parent.right) {                      // if z is right child: case 2
+                        z = z.parent;                               // move z up
+                        leftRotate(z);                              // rotate around z to create case 3
+                    }
+                                                                    // z is left child: case 3
+                    z.parent.color = Color.BLACK;                   // turn parent black: fixes rule 4, break rule 5
+                    z.parent.parent.color = Color.RED;              // turn grandparent red
+                    rightRotate(z.parent.parent);                   // and rotate: fix rule 5;
+                }
+            } else {
+                Node y = z.parent.parent.left;                      // y = uncle
+                if(y.color == Color.RED) {                          // uncle is red
+                    z.parent.color = Color.BLACK;                   // turn parent black: fixes rule 4, breaks rule 5
+                    y.color = Color.BLACK;                          // turn uncle black: fixes rule 4, break rule 5
+                    z.parent.parent.color = Color.RED;              // turn grandparent red: fixes rule 5
+                    z = z.parent.parent;                            // move z up 2 levels to check violation of rule 4
+                } else {
+                                                                    // if z.parent is a right child
+                                                                    // this needs to be inverted completely meaning
+                                                                    // case 2 (z is a right child) becomes case 3 (z is
+                                                                    // a left child) and vice versa
+                    if(z == z.parent.left) {
+                        z = z.parent;                               // move z up
+                        rightRotate(z);                             // rotate around z to create opposite case
+                    }
+                    z.parent.color = Color.BLACK;                   // turn parent black: fixes rule 4, breaks rule 5
+                    z.parent.parent.color = Color.RED;              // turn grandparent red
+                    leftRotate(z.parent.parent);                    // and rotate: fixes rule 5
+                }
+            }
+        }
+        root.color = Color.BLACK;
+    }
+
+    private void leftRotate(Node x) {
+        Node y = x.right;
+        x.right = y.left;
+        if (y.left != nil) {
+            y.left.parent = x;
+        }
+        y.parent = x.parent;
+        if (x.parent == nil) {
+            root = y;
+        } else if (x == x.parent.left) {
+            x.parent.left = y;
+        } else {
+            x.parent.right = y;
+        }
+        y.left = x;
+        x.parent = y;
+    }
+
+    private void rightRotate(Node x) {
+        Node y = x.left;
+        x.left = y.right;
+        if (y.right != nil) {
+            y.right.parent = x;
+        }
+        y.parent = x.parent;
+        if (x.parent == nil) {
+            root = y;
+        } else if (x == x.parent.left) {
+            x.parent.left = y;
+        } else {
+            x.parent.right = y;
+        }
+        y.right = x;
+        x.parent = y;
+    }
+
+    public static class Node {
+        enum Color {
+            RED, BLACK
+        }
+
+        Node left = null;
+        Node right = null;
+        Node parent = null;
+
+        Color color;
+
+        double key;
+
+        public Node(double key) {
+            this.key = key;
+            this.color = Color.RED;
+        }
     }
 
 }
---
+++ src/test/java/io/github/speciial/datastructures/redblacktree/RedBlackTreeTest.java
@@ -0,0 +1,54 @@
+package io.github.speciial.datastructures.redblacktree;
+
+import org.junit.jupiter.api.Test;
+
+import static io.github.speciial.datastructures.redblacktree.RedBlackTree.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RedBlackTreeTest {
+
+    // TODO: For now, this will just test against the API of the RedBlackTree but
+    //  there are some testing mechanisms to ensure that the structure of the
+    //  tree is correct and does not violate the rules. This will be included later.
+
+    @Test
+    void testSingleInsert() {
+        RedBlackTree tree = new RedBlackTree();
+        Node x = new Node(1.0);
+
+        tree.insert(x);
+
+        Node found = tree.search(x.key);
+        assertNotNull(found);
+        assertEquals(x.key, found.key);
+    }
+
+    @Test
+    void testMultipleInsert() {
+        RedBlackTree tree = new RedBlackTree();
+        Node x = new Node(16.0);
+        Node y = new Node(17.0);
+        Node z = new Node(18.0);
+        Node w = new Node(19.0);
+        Node v = new Node(99.0);
+
+        tree.insert(x);
+        tree.insert(y);
+        tree.insert(z);
+        tree.insert(w);
+        tree.insert(v);
+
+        Node foundX = tree.search(x.key);
+        assertNotNull(foundX);
+        assertEquals(x.key, foundX.key);
+
+        Node foundY = tree.search(y.key);
+        assertNotNull(foundY);
+        assertEquals(y.key, foundY.key);
+
+        Node foundV = tree.search(v.key);
+        assertNotNull(foundV);
+        assertEquals(v.key, foundV.key);
+    }
+
+}
